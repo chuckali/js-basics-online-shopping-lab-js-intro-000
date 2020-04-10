78eca4cbceee0ef7090e8d173dd1d16599ce697a
Charles Cusson <charlescusson@gmail.com>
2020-04-10 01:54:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,8 +40,8 @@ for(var i=0;i<cart.length;i++)
 
 function removeFromCart(item) {
 for(var i=0;i<cart.length;i++){
-if (cart[i].itemName===item){
-cart.splice(i+1,1)
+if (cart[i-1].itemName===item){
+cart.splice(i,1)
 return cart
 }
 else {
