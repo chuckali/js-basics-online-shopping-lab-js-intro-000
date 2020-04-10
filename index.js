dd6495d22c06270dc19e4c9882b80e3519cb12fb
Charles Cusson <charlescusson@gmail.com>
2020-04-10 02:06:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ for(var i=0;i<cart.length;i++){
 if (cart[i].itemName===item){
 cart.splice((i-1),1)
 return cart}
-if (i===cart.length-1) 
+if (i===cart.length-1)
 return `That item is not in your cart.`
 }
 }
