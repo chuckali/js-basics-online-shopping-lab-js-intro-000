c5db6c8755b59b2e75090c99d221db00af9eb160
Charles Cusson <charlescusson@gmail.com>
2020-04-10 02:20:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,10 +43,10 @@ for(var i=0;i<cart.length;i++){
 if (cart[i].itemName===item){
 cart.splice(i,1)
 return cart}
-else if (i===(cart.length-1)){
+}
 return `That item is not in your cart.`
 }
-}}
+
 
 function placeOrder(cardNumber) {
   // write your code here
